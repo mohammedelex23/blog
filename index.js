1255db900b4aab5bd5b8720b4fd008ca3a4b9128
mohammed ismail <warethco@gmail.com>
2020-07-29 16:50:59 +0200

    version 1 blog without database

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ app.use(bodyParser.urlencoded({extended : true}))
 
 var articles = [
     {
-        "name" : "Day 1",
-        "content": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt porro non velit omnis ea nulla nisi iste nemo maxime ex quis exercitationem mollitia aspernatur in illum, corporis cupiditate? Sapiente, eligendi."
+        "title" : "Day 1",
+        "post": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt porro non velit omnis ea nulla nisi iste nemo maxime ex quis exercitationem mollitia aspernatur in illum, corporis cupiditate? Sapiente, eligendi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt porro non velit omnis ea nulla nisi iste nemo maxime ex quis exercitationem mollitia aspernatur in illum, corporis cupiditate? Sapiente, eligendi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt porro non velit omnis ea nulla nisi iste nemo maxime ex quis exercitationem mollitia aspernatur in illum, corporis cupiditate? Sapiente, eligendi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt porro non velit omnis ea nulla nisi iste nemo maxime ex quis exercitationem mollitia aspernatur in illum, corporis cupiditate? Sapiente, eligendi."
     },
     {
-        "name" : "Day 2",
-        "content": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt porro non velit omnis ea nulla nisi iste nemo maxime ex quis exercitationem mollitia aspernatur in illum, corporis cupiditate? Sapiente, eligendi."
+        "title" : "Day 2",
+        "post": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt porro non velit omnis ea nulla nisi iste nemo maxime ex quis exercitationem mollitia aspernatur in illum, corporis cupiditate? Sapiente, eligendi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt porro non velit omnis ea nulla nisi iste nemo maxime ex quis exercitationem mollitia aspernatur in illum, corporis cupiditate? Sapiente, eligendi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt porro non velit omnis ea nulla nisi iste nemo maxime ex quis exercitationem mollitia aspernatur in illum, corporis cupiditate? Sapiente, eligendi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt porro non velit omnis ea nulla nisi iste nemo maxime ex quis exercitationem mollitia aspernatur in illum, corporis cupiditate? Sapiente, eligendi."
     }
 ]
 
@@ -23,9 +23,43 @@ app.get('/', (req, res) => {
     res.render('pages/home', { articles : articles })
 })
 
-// Create new Article 
+// Create new article page
 app.get('/create', (req, res) => {
     res.render('pages/create')
+})
+
+// Create new article
+app.post('/create', (req, res) => {
+
+    var title = req.body.title
+    var post = req.body.post
+
+    var article = articles.filter(article => article.title === title)
+
+    if (article.length === 0 || article === undefined) {
+        articles.push({
+            "title" : title,
+            "post" : post
+        })
+        res.redirect('/')
+    } else {
+        res.render('pages/errorPage', {"error" : "Article with this name already exist !"})
+    }
+
+    
+})
+
+// Serve specific article with name
+app.get('/:articleName', (req, res) => {
+    var articleName = req.params.articleName
+    var article = articles.filter((article) => article.title === articleName)
+
+    if (article.length === 0 || article === undefined) {
+        res.render('pages/errorPage', {"error" : "OOPS ! No article related to this name"})
+    } else {
+        console.log(article)
+        res.render('pages/article', { article : article })
+    }
 })
 
 
